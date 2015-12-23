018931780b6d5c2581e531e61ea3d60ecbc7a10b
Alessandro <alessandro@AleMac.local>
2015-12-23 19:18:12 +0100

    Typos and corrected syntax

--- cachematrix.R
+++ cachematrix.R
@@ -12,13 +12,14 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set <- finction (y)
+  set <- function (y) {
     x <<- y
     inv <<- NULL
-    get <- function() x
-    setinv <- function(inverse) inv <<- inverse
-    getinv <- function() inv
-    list(set = set, get = get,
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
 }
